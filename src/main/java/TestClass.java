a527360c794a099faf4d4ec501832889dbed8c80
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2021-12-05 12:13:51 +0200

    Initial commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass {
+
+
+    private final String name;
+
+    public TestClass(String name){
+
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
